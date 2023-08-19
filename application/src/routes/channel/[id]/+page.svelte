fc2a5d82bd46cd44b869de716ce93b8321a43c29
rladjswkd <dlfmaqkrrudah@gmail.com>
2023-08-19 20:24:25 +0900

    feat: /channel/[id] 경로 재접근 시 스벨트가 반응성을 갖도록 수정

--- application/src/routes/channel/[id]/+page.svelte
+++ application/src/routes/channel/[id]/+page.svelte
@@ -11,7 +11,7 @@
 	export let data;
 	console.log(data); //TODO: remove
 
-	const channelData = data.channelData;
+	$: channelData = data.channelData;
 	//TODO: 현재 로그인해있는 사용자의 이름을 받아와서 저장해서 이 페이지에서 반영구적으로 사용하는 방법 찾아보기 context api?
 	export let username: string = 'gyepark';
 
@@ -119,14 +119,14 @@
 	}
 </script>
 
-<div class="chat" bind:this={elemChat}>
+<div class="chat" bind:this="{elemChat}">
 	<section class="border-y border-surface-500/30 p-2.5 sticky top-0 bg-surface-100-800-token z-50">
 		<div class="grid grid-cols-[1fr_auto]">
 			<div class="flex items-center">
 				<div class="font-bold text-lg">{channelData.name}</div>
 			</div>
-			<button class="p-2 rounded-md hover:bg-surface-200-700-token" on:click={showChannelInfo}>
-				<div><i class="fa fa-user" aria-hidden="true" />{channelData._count.participants}</div>
+			<button class="p-2 rounded-md hover:bg-surface-200-700-token" on:click="{showChannelInfo}">
+				<div><i class="fa fa-user" aria-hidden="true"></i>{channelData._count.participants}</div>
 			</button>
 		</div>
 	</section>
@@ -179,19 +179,17 @@
 		<div class="input-group input-group-divider grid-cols-[auto_1fr_auto] rounded-container-token">
 			<button class="input-group-shim">+</button>
 			<textarea
-				bind:value={currentMessage}
+				bind:value="{currentMessage}"
 				class="bg-transparent border-0 ring-0"
 				name="prompt"
 				id="prompt"
 				placeholder=" Write a message..."
 				rows="1"
-				on:keypress={onPromptKeydown}
-			/>
+				on:keypress="{onPromptKeydown}"></textarea>
 			<button
-				class={currentMessage ? 'variant-filled-primary' : 'input-group-shim'}
-				on:click={addMessage}
-			>
-				<i class="fa fa-paper-plane" aria-hidden="true" />
+				class="{currentMessage ? 'variant-filled-primary' : 'input-group-shim'}"
+				on:click="{addMessage}">
+				<i class="fa fa-paper-plane" aria-hidden="true"></i>
 			</button>
 		</div>
 	</section>
