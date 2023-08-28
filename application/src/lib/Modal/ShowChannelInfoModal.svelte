7aabecf6b737857a5be3c4b1d288b3177fd60e73
rladjswkd <dlfmaqkrrudah@gmail.com>
2023-08-28 20:35:20 +0900

    feat: 현재 사용자가 채널 소유자면 채널의 관리자를 관리하는 탭을 보여주는 기능 구현

--- application/src/lib/Modal/ShowChannelInfoModal.svelte
+++ application/src/lib/Modal/ShowChannelInfoModal.svelte
@@ -4,13 +4,35 @@
 	import { BaseUrl } from '$lib/common';
 	import { getRequestApi } from '$lib/fetch';
 	import { onMount } from 'svelte';
+	import { update_await_block_branch } from 'svelte/internal';
+	
+	//TODO: 관리자에 대한 ban, kick, mute 가능해야 하나?
+
+	type User = {
+		nickname: string;
+		avatar: string;
+	}
+
+	type Content = {
+		isOwner: boolean;
+		participants: {
+			user: User;
+		}[];
+    administrators: {
+        user: User;
+    }[];
+	};
+
 	// Props
 	/** Exposes parent props to this component. */
 	export let parent: any;
 
-	let users: any[];
+	let participants: User[] = [];
+	let administrators: User[] = [];
+	let normalParticipants: User[] = [];
+	let content: Content = { isOwner: false, participants: [], administrators: [] };
 
-	let input: string;
+	let input: string; //TODO: 여러 구성 요소에서 동일한 변수인 input을 사용자의 입력을 받는데 사용하면 해당 구성 요소들 중 하나에 값을 입력했을 때 모든 구성 요소에 동일한 내용이 표시된다. 구성 요소마다 서로 다른 변수를 사용하도록 하자.
 	let tabSet: number = 0;
 	// Form Data
 	const formData = {
@@ -19,7 +41,10 @@
 	};
 
 	onMount(async () => {
-		users = await getRequestApi(BaseUrl.CHANNELS + `${$modalStore[0].meta.id}/participants`);
+		content = await getRequestApi(BaseUrl.CHANNELS + `${$modalStore[0].meta.id}/contents`);
+		participants = content.participants.map(p => p.user);
+		administrators = content.administrators.map(a => a.user);
+		normalParticipants = participants.filter(p => !administrators.some(a => a.nickname === p.nickname && a.avatar === p.avatar));
 	});
 
 	function showProfile(name: string): void {
@@ -43,6 +68,9 @@
 		<TabGroup>
 			<Tab bind:group="{tabSet}" name="member" value="{0}">정보</Tab>
 			<Tab bind:group="{tabSet}" name="member" value="{1}">멤버</Tab>
+			{#if administrators}
+				<Tab bind:group="{tabSet}" name="member" value="{2}">관리</Tab>
+			{/if}
 			<svelte:fragment slot="panel">
 				{#if tabSet === 0}
 					<div
@@ -57,7 +85,7 @@
 						<div class="{cChannelInfoTableHead}">생성날짜</div>
 						<div class="{cChannelInfoTableBody}">{$modalStore[0].meta.createdAt}</div>
 					</div>
-				{:else}
+				{:else if tabSet === 1}
 					<form class="modal-form {cForm}">
 						<label class="label">
 							<span class="font-bold">유저 이름</span>
@@ -73,7 +101,7 @@
 						<div class="{cUsers}">
 							<nav>
 								<ul>
-									{#each users as { nickname, avatar }, i}
+									{#each participants as { nickname, avatar }, i}
 										{#if i !== 0}
 											<hr />
 										{/if}
@@ -95,6 +123,83 @@
 							</nav>
 						</div>
 					</form>
+				{:else if tabSet === 2 && content.isOwner}
+					<div class="flex">
+						<form class="modal-form {cForm}">
+							<label class="label">
+								<span class="font-bold">일반 유저</span>
+								<div class="input-group input-group-divider grid-cols-[auto_1fr]">
+									<div class="input-group-shim"><i class="fa fa-search"></i></div>
+									<input
+										class="pl-2 py-1.5"
+										bind:value="{input}"
+										type="search"
+										placeholder="Enter the user name..." />
+								</div>
+							</label>
+							<div class="{cUsers}">
+								<nav>
+									<ul>
+										{#each normalParticipants as { nickname, avatar }, i}
+											{#if i !== 0}
+												<hr />
+											{/if}
+											<li>
+												<div
+													class="group w-full grid grid-cols-[1fr_auto] h-12 p-2 rounded-md hover:bg-surface-400">
+													<div class="flex items-center">
+														<Avatar src="{avatar}" width="w-6" rounded="rounded-md" />
+														<div class="ml-2">{nickname}</div>
+													</div>
+														<button
+															type="button"
+															class="btn btn-sm variant-filled hidden group-hover:block"
+															on:click="{() => showProfile(nickname)}">관리자추가</button>
+												</div>
+											</li>
+										{/each}
+									</ul>
+								</nav>
+							</div>
+						</form>
+						<form class="modal-form {cForm}">
+							<label class="label">
+								<span class="font-bold">관리자 이름</span>
+								<div class="input-group input-group-divider grid-cols-[auto_1fr]">
+									<div class="input-group-shim"><i class="fa fa-search"></i></div>
+									<input
+										class="pl-2 py-1.5"
+										bind:value="{input}"
+										type="search"
+										placeholder="Enter the user name..." />
+								</div>
+							</label>
+							<div class="{cUsers}">
+								<nav>
+									<ul>
+										{#each administrators as { nickname, avatar }, i}
+											{#if i !== 0}
+												<hr />
+											{/if}
+											<li>
+												<div
+													class="group w-full grid grid-cols-[1fr_auto] h-12 p-2 rounded-md hover:bg-surface-400">
+													<div class="flex items-center">
+														<Avatar src="{avatar}" width="w-6" rounded="rounded-md" />
+														<div class="ml-2">{nickname}</div>
+													</div>
+														<button
+															type="button"
+															class="btn btn-sm variant-filled hidden group-hover:block"
+															on:click="{() => showProfile(nickname)}">관리자제거</button>
+												</div>
+											</li>
+										{/each}
+									</ul>
+								</nav>
+							</div>
+						</form>
+					</div>
 				{/if}
 			</svelte:fragment>
 		</TabGroup>
