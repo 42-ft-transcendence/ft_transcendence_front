d5804c6deeb4ce3589963695112fb7b6006b103b
Eingerjar <cksrb1526@naver.com>
2023-09-08 21:02:55 +0900

    feat: join and leave room

--- application/src/routes/+layout.svelte
+++ application/src/routes/+layout.svelte
@@ -12,15 +12,26 @@
 	import { Modal } from '@skeletonlabs/skeleton';
 	import { activateProfile, userIdStore } from '$lib/store';
 	import { JWT_COOKIE_KEY, getCookie, hasCookie, socket } from '$lib/common';
+	import { page } from '$app/stores';
 
 
 	export let data;
 	let register = data.register;
 	let sidebarLeftBtn = false;
+	let preChanRoom: string | undefined = undefined; // default '/' ?
+	$:{
+		if (preChanRoom && preChanRoom.includes('/channel') || $page.url.pathname.includes('/game')){
+			socket.emit('leave Room', preChanRoom);
+		}
+		preChanRoom = $page.url.pathname;
+		if ($page.url.pathname.includes('/channel') || $page.url.pathname.includes('/game')){
+			socket.emit('join Room', $page.url.pathname);
+		}
+	}
 	//TODO: 인자가 -1이면 사용자 자기 자신의 프로필을 불러오도록 구현했었지만, 현재 사용자가 로그인할 시 현재 사용자의 id를 스토어에 저장해 활용할 것이므로, 이를 활용하도록 하자.
-	// function handleMyProfile() {
-	// 	activateProfile(-1); 
-	// }
+	function handleMyProfile() {
+		activateProfile(-1); 
+	}
 	if (hasCookie(JWT_COOKIE_KEY)){
 		socket.auth.token = `${getCookie(JWT_COOKIE_KEY)}`;
 		socket.connect();
