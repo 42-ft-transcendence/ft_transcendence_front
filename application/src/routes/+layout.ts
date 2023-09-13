a4216cec1a2076658bb777a50cbf966bd259827e
rladjswkd <dlfmaqkrrudah@gmail.com>
2023-09-13 20:01:02 +0900

    fix: +layout.ts 분기문 추가
    - goto 함수는 내부적으로 navigation 함수를 호출하는데, 이 함수는 작업을 마친후 호출한 위치로 돌아와 실행을 이어간다. 따라서 분기문으로 그 아래 코드들이 실행되지 않도록 막기 위함이다.

--- application/src/routes/+layout.ts
+++ application/src/routes/+layout.ts
@@ -21,48 +21,48 @@ import { get } from 'svelte/store';
 export const ssr = false;
 
 export async function load({ url }) {
-	if (['/login', '/signup', '/validate'].includes(url.pathname)) {
-		return generateReturnValueFallBack([], [], [], false);
-	}
-
+	if (['/login', '/signup', '/validate'].includes(url.pathname)) return;
+		
 	// before inserting user data into database
 	if (!hasCookie(JWT_OAUTH_KEY) && !hasCookie(JWT_DB_KEY)) await goto('/login');
 	else if (hasCookie(JWT_OAUTH_KEY)) await goto('/signup');
-	// after insertion but not two factor authenticated
-	// else {
+	else {
 		const { refresh } = await getRequestApi(BaseUrl.USERS + 'twoFactorSetting');
+		// after insertion but not two factor authenticated
 		if (refresh) await goto('/validate');
-		// after insertion and two factor authenticated
-		channelUserInStore.set(
-			(await getRequestApi(BaseUrl.CHANNELS + 'channelsUserIn')).map(
-				(channel: any) => ({ ...channel, href: `/channel/${channel.id}` }),
-			),
-		);
-		directUserInStore.set(
-			(await getRequestApi(BaseUrl.CHANNELS + 'directsUserIn')).map(
-				(direct: any) => ({ ...direct, href: `/channel/${direct.channelId}` }),
-			),
-		);
-		blockeeStore.set(
-			(await getRequestApi(BaseUrl.BLOCKED)).map((each: Blockee) => each.blockee),
-		);
-		followeeStore.set(
-			(await getRequestApi(BaseUrl.FOLLOWS)).map(
-				(each: Followee) => each.followee,
-			),
-		);
-
-		const user: UserProfile = await getRequestApi(BaseUrl.USERS + 'oneself');
-		userIdStore.set(user.id);
-		twoFactorAuthStore.set(user.is2FAEnabled);
-
-		return generateReturnValueFallBack(
-			get(channelUserInStore),
-			get(directUserInStore),
-			get(followeeStore),
-			true,
-		);
-	// }
+		else {
+			// after insertion and two factor authenticated
+			channelUserInStore.set(
+				(await getRequestApi(BaseUrl.CHANNELS + 'channelsUserIn')).map(
+					(channel: any) => ({ ...channel, href: `/channel/${channel.id}` }),
+				),
+			);
+			directUserInStore.set(
+				(await getRequestApi(BaseUrl.CHANNELS + 'directsUserIn')).map(
+					(direct: any) => ({ ...direct, href: `/channel/${direct.channelId}` }),
+				),
+			);
+			blockeeStore.set(
+				(await getRequestApi(BaseUrl.BLOCKED)).map((each: Blockee) => each.blockee),
+			);
+			followeeStore.set(
+				(await getRequestApi(BaseUrl.FOLLOWS)).map(
+					(each: Followee) => each.followee,
+				),
+			);
+	
+			const user: UserProfile = await getRequestApi(BaseUrl.USERS + 'oneself');
+			userIdStore.set(user.id);
+			twoFactorAuthStore.set(user.is2FAEnabled);
+	
+			return generateReturnValueFallBack(
+				get(channelUserInStore),
+				get(directUserInStore),
+				get(followeeStore),
+				true,
+			);
+		}
+	}
 }
 
 function generateReturnValueFallBack(
