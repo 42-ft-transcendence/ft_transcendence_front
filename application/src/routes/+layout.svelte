f6f72edb7d5576f2896033dbb559bbd3c9e86020
Eingerjar <cksrb1526@naver.com>
2023-09-18 07:22:59 +0900

    feat: join channel

--- application/src/routes/+layout.svelte
+++ application/src/routes/+layout.svelte
@@ -41,12 +41,10 @@
 			socket.emit('leave Room', preChanRoom);
 		}
 		preChanRoom = $page.url.pathname;
-		if (
-			$page.url.pathname.includes('/channel') ||
-			$page.url.pathname.includes('/game')
-		) {
+		if ($page.url.pathname.includes('/channel'))
+			socket.emit('join Channel', {channelId: $page.params.id});
+		else if ($page.url.pathname.includes('/game'))
 			socket.emit('join Room', $page.url.pathname);
-		}
 	}
 	//TODO: 인자가 -1이면 사용자 자기 자신의 프로필을 불러오도록 구현했었지만, 현재 사용자가 로그인할 시 현재 사용자의 id를 스토어에 저장해 활용할 것이므로, 이를 활용하도록 하자.
 
